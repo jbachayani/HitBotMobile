7422aa659c32200ce29090f2836d78244394c112
Jérémy Bachayani <jeremy.bachayani@gmail.com>
2018-03-20 14:31:28 +0100

    Alexa wathing you

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {ApiAiClient} from "api-ai-javascript";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
 export class HomePage {
-
+  developerAccessToken = 'd6d51ec9cacf494abb638bfad93a95bc';
+  
   constructor(public navCtrl: NavController) {
+    let client = new ApiAiClient({accessToken: this.developerAccessToken})
+    .textRequest('Hello man !');
 
+    console.log(client);
   }
-
 }
